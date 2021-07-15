4c2eeb121a8e9cf3c788076d6125454f08850ad4
FrancoForna <ffornaa@gmail.com>
2021-07-14 23:38:44 -0300

    cambios al carrito cart

--- src/components/CarritoContainer/index.jsx
+++ src/components/CarritoContainer/index.jsx
@@ -1,17 +1,27 @@
 import { useContext } from "react";
 import { ShopContext } from "../../context/ShopContext";
+import { Link } from "react-router-dom";
 
 export const CarritoContainer = () => {
   const { cart, clearCart, removeItem } = useContext(ShopContext);
+  console.log(cart.length);
   return (
     <>
+      {cart.length !== 0 || (
+        <div>
+          No tienes productos en el carrito!
+          <Link to="/">
+            <button> Volver a inicio</button>
+          </Link>{" "}
+        </div>
+      )}
       {cart.map((product) => {
         return (
           <>
             <div>
               <div>producto:{product.item.name}</div>
               <div>cantidad:{product.cantidad}</div>
-              <button onClick={() => removeItem(product)}>Elminar item</button>
+              <button onClick={() => removeItem(product)}>Eliminar item</button>
             </div>
           </>
         );
